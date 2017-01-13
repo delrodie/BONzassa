117f2b698a3037c66e10ec3596a6f8fde2bec789
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2017-01-13 12:05:22 +0000

    Mise en page de la page accueil

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -22,10 +22,86 @@ class DefaultController extends Controller
     }
 
     /**
-     * @Route("/test", name="test")
+     * Slider
      */
-    public function presentationAction(Request $request)
+    public function sliderAction()
     {
-        $article = new Presentation();
+        $em = $this->getDoctrine()->getManager();
+
+        $sliders = $em->getRepository('AppBundle:Slider')->findAll();
+
+        return $this->render('fr/slider.html.twig', array(
+            'sliders' => $sliders,
+        ));
+    }
+
+    /**
+     * @Route("/accueil_presentation", name="fr_accueil_presentation")
+     */
+    public function accueilpresentationAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $presentations = $em->getRepository('AppBundle:Accueil')->findAll();
+
+        return $this->render('fr/accueil_presentation.html.twig', array(
+            'presentations' => $presentations,
+        ));
+    }
+
+    /**
+     * @Route("/accueil_menu_presentation", name="fr_accueil_menu_presentation")
+     */
+    public function amenupresentationAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $presentations = $em->getRepository('AppBundle:Presentation')->findAll();
+
+        return $this->render('fr/amenu_presentation.html.twig', array(
+            'presentations' => $presentations,
+        ));
+    }
+
+    /**
+     * @Route("/accueil_menu_avantage", name="fr_accueil_menu_avantage")
+     */
+    public function amenuavantageAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $avantages = $em->getRepository('AppBundle:Avantage')->findAll();
+
+        return $this->render('fr/amenu_avantage.html.twig', array(
+            'avantages' => $avantages,
+        ));
+    }
+
+    /**
+     * @Route("/accueil_menu_communaute", name="fr_accueil_menu_communaute")
+     */
+    public function amenucommunauteAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $communautes = $em->getRepository('AppBundle:Communaute')->findAll();
+
+        return $this->render('fr/amenu_communaute.html.twig', array(
+            'communautes' => $communautes,
+        ));
+    }
+
+    /**
+     * @Route("/accueil_menu_contact", name="fr_accueil_menu_contact")
+     */
+    public function amenucontactAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $contacts = $em->getRepository('AppBundle:Contact')->findAll();
+
+        return $this->render('fr/amenu_contact.html.twig', array(
+            'contacts' => $contacts,
+        ));
     }
 }
